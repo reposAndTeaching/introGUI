4ffea52c186e3e6877aa7db54c067120ae951f46
dotCero <lucero.inginfo@gmail.com>
2021-10-14 13:35:50 -0300

    Agregar archivos de proyecto.

---
+++ introGUI/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace introGUI
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //DRY => Dont repeat yourself
+        List<Pelicula> listaPeliculas;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Title = "Título desde código";
+            listaPeliculas = new List<Pelicula>();
+            listaPeliculas.Add(new Pelicula("Iron-Man", 2008, true));
+
+            actualizarListado();
+        }
+
+        private void buttonRegistrar_Click(object sender, RoutedEventArgs e)
+        {
+            string nombreCaja = textBoxNombre.Text;
+            int anioCaja = Convert.ToInt32(textBoxAnio.Text);
+            var recomiendaCaja = checkBoxRecomienda.IsChecked;
+
+            Pelicula p = new Pelicula(nombreCaja, anioCaja, (bool)recomiendaCaja);
+            listaPeliculas.Add(p);
+
+            actualizarListado();
+
+            textBoxNombre.Text = "";
+            textBoxAnio.Text = "";
+            checkBoxRecomienda.IsChecked = false;
+
+            textBoxNombre.Focus();
+        }
+
+        private void actualizarListado()
+        {
+            string textoLabel = "";
+            foreach (Pelicula pelicula in listaPeliculas)
+            {
+                textoLabel += pelicula;
+                textoLabel += "\n";
+            }
+            labelLista.Content = textoLabel;
+        }
+
+
+    }
+}
---
+++ introGUI/Pelicula.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace introGUI
+{
+    class Pelicula
+    {
+        private string nombre;
+        private int anio;
+        private bool recomendada;
+
+        public Pelicula(string nombre, int anio, bool recomendada)
+        {
+            this.nombre = nombre;
+            this.anio = anio;
+            this.recomendada = recomendada;
+        }
+
+        public Pelicula(string nombre, int anio)
+        {
+            this.nombre = nombre;
+            this.anio = anio;
+            this.recomendada = false;
+        }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int Anio { get => anio; set => anio = value; }
+        public bool Recomendada { get => recomendada; set => recomendada = value; }
+
+        public override string ToString()
+        {
+            string recomiendo = "";
+
+            if(recomendada)
+            {
+                recomiendo = "- Película Recomendada";
+            }
+
+            return nombre + " - " + anio + recomiendo;
+        }
+    }
+}
